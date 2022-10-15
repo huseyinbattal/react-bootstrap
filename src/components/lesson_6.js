132e07ee1240c2df7d10953ca0cab7a2f2b87a0f
Hüseyin BATTAL <95706081+huseyinbattal@users.noreply.github.com>
2022-10-15 16:19:23 +0300

    add stack

--- src/components/lesson_6.js
+++ src/components/lesson_6.js
@@ -5,24 +5,11 @@ const Lesson_6 = () => {
   const renderGrid = () => {
     return (
       <>
-        <Badge bg="warning" className="rounded-circle">
-          1
-        </Badge>{" "}
-        <Badge bg="success" className="rounded-circle">
-          2
-        </Badge>{" "}
-        <Badge bg="danger" className="rounded-circle">
-          3
-        </Badge>{" "}
-        <Badge bg="secondary" className="rounded-circle">
-          4
-        </Badge>{" "}
-        <Badge bg="dark" className="rounded-circle">
-          5
-        </Badge>{" "}
-        <Badge bg="primary" className="rounded-circle">
-          6
-        </Badge>
+       <Stack gap={3}>
+      <div className="bg-light border">First item</div>
+      <div className="bg-light border">Second item</div>
+      <div className="bg-light border">Third item</div>
+    </Stack>
       </>
     );
   };
